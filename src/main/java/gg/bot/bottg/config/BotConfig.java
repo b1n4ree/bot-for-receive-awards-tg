3b1a3767bb0afd0b719246805c9a05babd9c570b
alex <ya.ya.zhuck.global@gmail.com>
2023-04-20 16:09:40 +0300

    init

--- src/main/java/gg/bot/bottg/config/BotConfig.java
+++ src/main/java/gg/bot/bottg/config/BotConfig.java
@@ -1,6 +1,7 @@
 package gg.bot.bottg.config;
 
 import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +11,12 @@ import static java.lang.System.getProperty;
 @Configuration
 public class BotConfig {
 
+    @Value("${bot_token}")
+    private String botToken;
 
     @Bean
     public TelegramBot getTelegramBot() {
-        return new TelegramBot("5834417527:AAG8gchW6DfhHswxWv1jJexhuLP8IT6yfdk");
+        return new TelegramBot(botToken);
     }
 
     @Bean
